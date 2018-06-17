7076d60dfbc07d88f136320cb5e206b568d39761
Laurenz <laurenz.nitsche@gmail.com>
2018-06-17 18:51:50 +0200

    cy

--- KAMPF.java
+++ KAMPF.java
@@ -1,5 +1,5 @@
 import java.util.Random;
-
+ 
 public class KAMPF
 {
     GRAFIKELEMENTE grafik;
--- WELT.java
+++ WELT.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+import java.util.Random; 
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.*;
