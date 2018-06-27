2164aa063e71a2afe0861107a4438e64a014a8a1
Philipp <philipp.opheys@outlook.de>
2018-06-28 00:57:31 +0200

    Config Update

--- CONFIG.java
+++ CONFIG.java
@@ -3,15 +3,19 @@ public class CONFIG
     /**Bogenschütze**/  
     public static int HP0= 40;
     public static int Dmg0= 50;
+    
     /**Speerkämpfer**/  
     public static int HP1= 60;
     public static int Dmg1= 40;
+    
     /**Schildknappe**/  
     public static int HP2= 75;
     public static int Dmg2= 20;
+    
     /**Ritter**/  
     public static int HP3= 60;
     public static int Dmg3= 30;
+    
     /**Paladin**/  
     public static int HP4= 100;
     public static int Dmg4= 35;
